2444ddbedc94e66d4eb66ad2758fe169b37bb757
Debu Patra <patradebu1207@gmail.com>
2021-07-12 16:26:54 +0530

    updating main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "7ad2d8ahdo179ad2hd0awodnq2"
 
 
 @app.route("/")
@@ -9,4 +10,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
